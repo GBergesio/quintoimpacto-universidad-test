25639813a72c9b12c69288f3dd9f911e686d1089
Guillermo Bergesio <91171763+GBergesio@users.noreply.github.com>
2023-07-04 20:01:54 -0300

    Initial commit

---
+++ test-tecnico/src/main/java/QuintoImpacto/testtecnico/TestTecnicoQuintoImpactoApplication.java
@@ -0,0 +1,13 @@
+package QuintoImpacto.testtecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTecnicoQuintoImpactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTecnicoQuintoImpactoApplication.class, args);
+	}
+
+}
---
+++ test-tecnico/src/test/java/QuintoImpacto/testtecnico/TestTecnicoQuintoImpactoApplicationTests.java
@@ -0,0 +1,13 @@
+package QuintoImpacto.testtecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTecnicoQuintoImpactoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
